fae66e9e7e9d5e3d530f55d5d5128bdfff12a8fd
aryan757 <57763333+aryan757@users.noreply.github.com>
2021-04-02 18:06:50 +0530

    Update login.dart

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app4/ui/signup.dart';
 import 'package:flutter_app4/utils/auth_helper.dart';
@@ -23,59 +24,120 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              const SizedBox(height: 100.0),
-              Text(
-                "Login",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+              Container(
+                //jatin
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/frame.png',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              const SizedBox(height: 50.0),
+              Container(
+                //jatin
+                margin: EdgeInsets.only(left: 20),
+                alignment: Alignment.topLeft,
+                child: Text("Login",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w400)),
               ),
               const SizedBox(height: 20.0),
-              RaisedButton(
-                child: Text("Login with Google"),
-                onPressed: () async {
-                  try {
-                    await AuthHelper.signInWithGoogle();
-                  } catch (e) {
-                    print(e);
-                  }
-                },
-              ),
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(hintText: "Enter email"),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Center(
+                  child: TextFormField(
+                    controller: _emailController,
+                    autocorrect: true,
+                    maxLines: 1,
+                    keyboardType: TextInputType.emailAddress,
+                    autofocus: false,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                          width: 1.2,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.yellow,
+                          width: 1.2,
+                        ),
+                      ),
+                      border: InputBorder.none,
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      hintText: 'Email',
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
               ),
               const SizedBox(height: 10.0),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(hintText: "Enter password"),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Center(
+                  child: TextFormField(
+                    controller: _passwordController,
+                    autocorrect: true,
+                    maxLines: 1,
+                    autofocus: false,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                          width: 1.2,
+                        ),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.yellow,
+                          width: 1.2,
+                        ),
+                      ),
+                      border: InputBorder.none,
+                      floatingLabelBehavior: FloatingLabelBehavior.auto,
+                      hintText: 'Password',
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
               ),
               const SizedBox(height: 10.0),
-              RaisedButton(
-                child: Text("Login"),
-                onPressed: () async {
-                  if (_emailController.text.isEmpty ||
-                      _passwordController.text.isEmpty) {
-                    print("Email and password cannot be empty");
-                    return;
-                  }
-                  try {
-                    final user = await AuthHelper.signInWithEmail(
-                        email: _emailController.text,
-                        password: _passwordController.text);
-                    if (user != null) {
-                      print("login successful");
+              Container(
+                alignment: Alignment.center,
+                child: RaisedButton(
+                  elevation: 3,
+                  color: Colors.blueAccent,
+                  child: Text("Login"),
+                  onPressed: () async {
+                    if (_emailController.text.isEmpty ||
+                        _passwordController.text.isEmpty) {
+                      print("Email and password cannot be empty");
+                      return;
                     }
-                  } catch (e) {
-                    print(e);
-                  }
-                },
+                    try {
+                      final user = await AuthHelper.signInWithEmail(
+                          email: _emailController.text,
+                          password: _passwordController.text);
+                      if (user != null) {
+                        print("login successful");
+                      }
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                ),
               ),
-              RaisedButton(
+
+              TextButton(
                 child: Text("Create Account"),
                 onPressed: () async {
                   Navigator.push(
