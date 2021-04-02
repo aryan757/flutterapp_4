57cdb784d6af099438dc7328513f002068698bbf
aryan757 <57763333+aryan757@users.noreply.github.com>
2021-04-02 18:09:07 +0530

    Update admin_home.dart

--- lib/ui/admin_home.dart
+++ lib/ui/admin_home.dart
@@ -8,7 +8,7 @@ class AdminHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Admin Home'),
+        title: Text('AdminHome'),
       ),
       body: SingleChildScrollView(
         child: Column(
